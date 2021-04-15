52300eee2c66767c01b28358766e8440e90f6dd6
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-04-15 00:22:25 -0400

    add Error view

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -4,11 +4,33 @@ namespace Application\Controllers;
 
 use DevNet\Web\Mvc\Controller;
 use DevNet\Web\Mvc\IActionResult;
+use Exception;
 
 class HomeController extends Controller
 {
     public function index() : IActionResult
     {
         return $this->view();
+    }
+
+    public function error()
+    {
+        $error = new Exception("Sorry! Looks like this page doesn't exist.", 404);
+        if($this->HttpContext->Error)
+        {
+            switch ($this->HttpContext->Error->getCode())
+            {
+                case 404:
+                    break;
+                case 403:
+                    $error = new Exception("Sorry! Your request is not allowed.", 403);
+                    break;
+                default:
+                    $error = new Exception("Woops! Looks like something went wrong.", 500);
+                    break;
+            }
+        }
+
+        return $this->view($error);
     }
 }
