e6f5ecd48abc39e5be6c736d535fba3b02998963
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-05-31 04:52:01 -0400

    add RegisterForm

---
+++ Models/RegisterForm.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Models;
+
+class RegisterForm
+{
+    public string $Name;
+    public string $Email;
+    public string $Password;
+
+    public function __get(string $name)
+    {
+        return $this->$name;
+    }
+
+    public function __set(string $name, $value)
+    {
+         $this->$name = $value;
+    }
+
+    public function isValide() : bool
+    {
+        if (empty($this->Name) || empty($this->Email) || empty($this->Password))
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
