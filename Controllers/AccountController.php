d7d52941f6c6fe635653eb20ab9df5f00dd37359
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-06-01 05:12:51 -0400

    add json registration and login logic

--- Controllers/AccountController.php
+++ Controllers/AccountController.php
@@ -2,15 +2,26 @@
 
 namespace Application\Controllers;
 
+use DevNet\System\Linq;
+use DevNet\System\Type;
+use DevNet\System\Collections\ArrayList;
 use DevNet\Web\Mvc\Controller;
 use DevNet\Web\Mvc\IActionResult;
 use DevNet\Web\Mvc\Filters\AuthorizeFilter;
+use DevNet\Web\Mvc\Filters\AntiForgeryFilter;
 use DevNet\Web\Security\ClaimsPrincipal;
 use DevNet\Web\Security\ClaimsIdentity;
 use DevNet\Web\Security\ClaimType;
 use DevNet\Web\Security\Claim;
 use Application\Models\LoginForm;
+use Application\Models\RegisterForm;
+use Application\Models\User;
 
+/**
+ * This is an example on how to create registration and login system using claims without SQL database.
+ * This example dosen't encrypt your data, so it's not recommanded for production,
+ * Use DevNet Identity Manager instead, or encrypt you own data.
+ */
 class AccountController extends Controller
 {
     public function __construct()
@@ -33,7 +44,7 @@ class AccountController extends Controller
 
         if ($user->isAuthenticated())
         {
-            return $this->redirect('account/index');
+            return $this->redirect('/account/index');
         }
 
         if (!$form->isValide())
@@ -41,25 +52,65 @@ class AccountController extends Controller
             return $this->view();
         }
 
-        $identity = new ClaimsIdentity('AuthenticationUser');
-        $identity->addClaim(new Claim(ClaimType::Email, $form->Username));
-        $identity->addClaim(new Claim(ClaimType::Role, 'Admin'));
-        $user = new ClaimsPrincipal($identity);
-        $authentication = $this->HttpContext->Authentication;
-        $authentication->SignIn($user, $form->Remember);
+        $json = file_get_contents(__DIR__.'/../data.json');
+        $data = json_decode($json);
 
-        return $this->redirect('account/index');
+        $users = new ArrayList(Type::Object);
+        $users->addrange($data);
+
+        $user = $users->where(fn($user) => $user->Username == $form->Username)->first();
+
+        if (!$user)
+        {
+            return $this->view();
+        }
+
+        if ($user->Password != $form->Password)
+        {
+            return $this->view();
+        }
+
+        $identity = new ClaimsIdentity('AuthenticationUser');
+        $identity->addClaim(new Claim(ClaimType::Name, $user->Name));
+        $identity->addClaim(new Claim(ClaimType::Email, $user->Username));
+        $identity->addClaim(new Claim(ClaimType::Role, 'Memeber'));
+        $userPrincipal  = new ClaimsPrincipal($identity);
+        $authentication = $this->HttpContext->Authentication;
+        $authentication->SignIn($userPrincipal, $form->Remember);
+
+        return $this->redirect('/account/index');
+    }
+
+    public function register(RegisterForm $form) : IActionResult
+    {
+        if (!$form->isValide())
+        {
+            return $this->view();
+        }
+
+        $data = [];
+        if (file_exists(__DIR__.'/../data.json'))
+        {
+            $json = file_get_contents(__DIR__.'/../data.json');
+            $data = json_decode($json, true);
+        }
+
+        $user = new User();
+        $user->Name = $form->Name;
+        $user->Username = $form->Email;
+        $user->Password = $form->Password;
+
+        $data[] = $user;
+        $json   = json_encode($data, JSON_PRETTY_PRINT);
+        file_put_contents(__DIR__.'/../data.json', $json);
+
+        return $this->content("User {$user->Name} was created successfully, try to login to your account");
     }
 
     public function logout() : IActionResult
     {
         $authentication = $this->HttpContext->Authentication;
         $authentication->SignOut();
-        return $this->redirect('account/login');
-    }
-
-    public function register() : IActionResult
-    {
-        return $this->view();
+        return $this->redirect('/account/login');
     }
 }
