01b61b053015c244e9b7bec73a29d0295541b5bd
Mohammed Moussaoui <42718534+Artister@users.noreply.github.com>
2021-11-07 17:48:03 -0500

    update using IApplicationBuilder namespace

--- Startup.php
+++ Startup.php
@@ -4,7 +4,7 @@ namespace Application;
 
 use DevNet\Core\Configuration\IConfiguration;
 use DevNet\Core\Dependency\IServiceCollection;
-use DevNet\Core\Dispatcher\IApplicationBuilder;
+use DevNet\Core\Middleware\IApplicationBuilder;
 use DevNet\Core\Extensions\ServiceCollectionExtensions;
 use DevNet\Core\Extensions\ApplicationBuilderExtensions;
 
